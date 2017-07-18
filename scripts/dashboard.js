a5381bcfd820cce90e7ef8da3be1c4978de899ae
Alex Tolmach <ext_tolmach@kaspersky.com>
2017-07-18 15:31:50 +0300

    Map Controls now work

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -2,13 +2,56 @@ function formatDate(date){
 	return d3.timeFormat("%d.%m.%Y")(date);
 }
 
+function zeros(dimensions) {
+  var array = [];
+  for (var i = 0; i < dimensions[0]; ++i) {
+    array.push(dimensions.length == 1 ? 0 : zeros(dimensions.slice(1)));
+  }
+  return array;
+}
+
+function circle2zeros(x,y){
+  var zer = zeros([128, 128]),
+    circle = [3, 6, 8, 9, 10, 11, 11, 12, 12, 12, 13, 13, 13, 13, 13, 13, 13, 12, 12, 12, 11, 11, 10, 9, 8, 6, 3];
+  for(var i=0; i<circle.length; i++){
+    for(var j=-1*circle[i]; j<circle[i]; j++){
+      var y_coord = y-13+i,
+        x_coord = x+j;
+      if (y_coord >= 0 & y_coord < 128 & x_coord >= 0 & x_coord < 128) {
+        zer[y_coord][x_coord] = 1;
+      }
+    }
+  }
+  return zer;
+}
+
 function Map(container_id){
   var self = this;
   self.container = container_id;
   self.svg = d3.select(this.container);
   self.raster = this.svg.append("g");
+  /*
+  self.vector = this.svg.append("canvas")
+    .attr("id", "heatmap")
+    .attr("width", 128)
+    .attr("height", 128)
+    .style("width", "100%")
+    .style("height", "100%");
+  */
+
   self.vector = this.svg.append("g");
+  self.vector.append("image");
   self.tile = d3.tile();
+
+  self.fog_filter = {
+    "win"  : [true, false],
+    "side" : ["Radiant", "Dire"],
+    "ward_type" : ["obs_log", "sen_log"],
+    "start_time" : [new Date(0), new Date()],
+    "ward_time"  : [-100, 10000],
+    "playername" : [],
+    "heroname"   : []
+  };
 
   this.setSize = function(){
     var that = this;
@@ -42,6 +85,9 @@ function Map(container_id){
       .translate([transform.x, transform.y])
       ();
 
+    self.vector
+      .attr("transform", self.stringify_vec(tiles.scale, tiles.translate, tiles[0][2]));
+
     var image = self.raster
       .attr("transform", self.stringify(tiles.scale, tiles.translate, true))
       .selectAll("image")
@@ -66,25 +112,94 @@ function Map(container_id){
     }
   }
 
+  this.stringify_vec = function(scale, translate, scal2) {
+    var k = scale / 256, r = scale % 1 ? Number : Math.round;
+    return "translate(" + r(translate[0] * scale) + "," + r(translate[1] * scale) + ") scale(" + (k * (2**(scal2+1))) + ")";
+  }
+
   this.getData = function(){
     d3.csv("data/wards.csv", function(error, wards) {
       if (error) throw error;
-      self.wards_data = wards;
+      wards = wards.map(function(item){
+        item.start_time = new Date(parseInt(item.start_time)*1000);
+        item.x = parseInt(item.x);
+        item.y = parseInt(item.y);
+        item.ward_start_time = parseInt(item.ward_start_time);
+        item.ward_end_time = parseInt(item.ward_end_time);
+        item.win = item.win == "true";
+        return item;
+      });
+      self.all_wards = wards.map(self.addCirclesToData);
+      self.wards_data = self.all_wards;
       self.updateFilters();
+      self.toPlotArray();
+      self.drawFog();
     });
+  }
+
+  this.redrawFog = function(){
+    self.wards_data = self.all_wards.reduce(function(acc, x){
+      if(self.fog_filter.win.indexOf(x.win) >= 0 & self.fog_filter.side.indexOf(x.side) >= 0 & self.fog_filter.ward_type.indexOf(x.ward_type) >= 0 & self.fog_filter.playername.indexOf(x.playername) >= 0 & self.fog_filter.heroname.indexOf(x.heroname) >= 0 & self.fog_filter.start_time[0] <= x.start_time & x.start_time <= self.fog_filter.start_time[1] & self.fog_filter.ward_time[0] <= x.ward_end_time & x.ward_start_time <= self.fog_filter.ward_time[1]){
+        acc.push(x);
+      };
+      return acc;
+    }, []);
+    self.toPlotArray();
+    self.drawFog();
+  }
+
+  this.addCirclesToData = function(datapoint){
+    datapoint.y = 128 - datapoint.y;
+    var zarr = circle2zeros(datapoint.x, datapoint.y);
+    datapoint.arr = zarr;
+    return datapoint;
+  }
+
+  this.toPlotArray = function(){
+    var acc = zeros([128, 128]);
+    self.fog_data = self.wards_data.reduce(function(acc, x){
+      for(i=0;i<128;i++){
+        for(j=0;j<128;j++){
+          acc[j][i] += x.arr[j][i];
+        }
+      }
+      return acc;
+    }, acc);
+    self.fog_data = self.fog_data.reduce(function(a, b){ return a.concat(b); }, []);
+  }
+
+  this.drawFog = function(){
+    var canvas = document.createElement("canvas");
+    canvas.width=128;
+    canvas.height=128;
+    var color = d3.scaleSequential().domain([1, d3.max(self.fog_data)]).interpolator(d3.interpolateWarm);
+    var context = canvas.getContext("2d"),
+      image = context.createImageData(128, 128);
+    //console.log(d3.max(self.fog_data));
+
+    for (var j = 0, k = 0, l = 0; j < 128; ++j) {
+      for (var i = 0; i < 128; ++i, ++k, l += 4) {
+        var c = d3.rgb(color(self.fog_data[k]));
+        image.data[l + 0] = c.r;
+        image.data[l + 1] = c.g;
+        image.data[l + 2] = c.b;
+        if(self.fog_data[k]==0){
+          image.data[l + 3] = 0;
+        }else{
+          image.data[l + 3] = 100;
+        }
+        
+      }
+    }
+
+    context.putImageData(image, 0, 0);
+
+    self.vector.select("image")
+      .attr("href", canvas.toDataURL());
   }
 
   this.updateFilters = function(){
     var that = this;
-    self.wards_data = self.wards_data.map(function(item){
-      item.start_time = new Date(parseInt(item.start_time)*1000);
-      item.x = parseInt(item.x);
-      item.y = parseInt(item.y);
-      item.ward_start_time = parseInt(item.ward_start_time);
-      item.ward_end_time = parseInt(item.ward_end_time);
-      item.win = item.win == "true";
-      return item;
-    });
     filter_limits = self.wards_data.reduce(function(acc, cur){
       if( cur.start_time < acc.dates[0] ){
         acc.dates[0] = cur.start_time;
@@ -125,11 +240,56 @@ function Map(container_id){
     });
     filter_limits.players = filter_limits.players.map(function(x){ return x[0]; });
 
+    d3.select("fieldset#result-checkboxes").selectAll("input").on("change", function(){
+      var filter = d3.select("fieldset#result-checkboxes")
+        .selectAll("input").nodes()
+        .map(function(x){ return [$(x).checkboxradio("option", "label"), $(x).is(":checked")]; });
+      filter = filter.reduce(function(acc, x){
+        if(x[0]=="Win" & x[1]){
+          acc.push(true);
+        }else if(x[0]=="Loose" & x[1]){
+          acc.push(false);
+        }
+        return acc;
+      }, []);
+      self.fog_filter.win = filter;
+      self.redrawFog();
+    });
+
+    d3.select("fieldset#team-checkboxes").selectAll("input").on("change", function(){
+      var filter = d3.select("fieldset#team-checkboxes")
+        .selectAll("input").nodes()
+        .reduce(function(acc, x){
+          if($(x).is(":checked")){
+            acc.push($(x).checkboxradio("option", "label"));
+          };
+          return acc;
+        }, []);
+      self.fog_filter.side = filter;
+      self.redrawFog();
+    });
+
+    d3.select("fieldset#wardtype-checkboxes").selectAll("input").on("change", function(){
+      var filter = d3.select("fieldset#wardtype-checkboxes")
+        .selectAll("input").nodes()
+        .reduce(function(acc, x){
+          if($(x).is(":checked")){
+            acc.push($(x).checkboxradio("option", "label").slice(0, 3).toLowerCase() + "_log");
+          };
+          return acc;
+        }, []);
+      self.fog_filter.ward_type = filter;
+      self.redrawFog();
+    });
+
+    self.fog_filter.playername = filter_limits.players;
+
     d3.select("fieldset#players-checkboxes").selectAll("label").data(filter_limits.players)
       .enter()
         .append("label")
           .attr("for", function(d, i){ return "player-"+i; })
           .text(function(d){ return d });
+
 
     d3.select("fieldset#players-checkboxes").selectAll("input").data(filter_limits.players)
       .enter()
@@ -137,7 +297,21 @@ function Map(container_id){
           .attr("type", "checkbox")
           .attr("checked", "")
           .attr("name", function(d, i){ return "player-"+i; })
-          .attr("id", function(d, i){ return "player-"+i; });
+          .attr("id", function(d, i){ return "player-"+i; })
+          .on("change", function(){
+            var filter = d3.select("fieldset#players-checkboxes")
+              .selectAll("input").nodes()
+              .reduce(function(acc, x){
+                if($(x).is(":checked")){
+                  acc.push($(x).checkboxradio("option", "label"));
+                };
+                return acc;
+              }, []);
+            self.fog_filter.playername = filter;
+            self.redrawFog();
+          });
+
+    self.fog_filter.heroname = filter_limits.heroes;
 
     d3.select("fieldset#heroes-checkboxes").selectAll("label").data(filter_limits.heroes)
       .enter()
@@ -151,7 +325,19 @@ function Map(container_id){
           .attr("type", "checkbox")
           .attr("checked", "")
           .attr("name", function(d, i){ return "hero-"+i; })
-          .attr("id", function(d, i){ return "hero-"+i; });
+          .attr("id", function(d, i){ return "hero-"+i; })
+          .on("change", function(){
+            var filter = d3.select("fieldset#heroes-checkboxes")
+              .selectAll("input").nodes()
+              .reduce(function(acc, x){
+                if($(x).is(":checked")){
+                  acc.push($(x).checkboxradio("option", "label"));
+                };
+                return acc;
+              }, []);
+            self.fog_filter.heroname = filter;
+            self.redrawFog();
+          });
 
     $( "input" ).checkboxradio();
 
@@ -162,10 +348,11 @@ function Map(container_id){
       step: 86400,
       values: [ filter_limits.dates[0] / 1000, filter_limits.dates[1] / 1000 ],
       slide: function( event, ui ) {
-        $( "#match-date-range" ).text(
-          formatDate(new Date(ui.values[ 0 ] *1000)) + " - " + 
-          formatDate(new Date(ui.values[ 1 ] *1000))
-        );
+        var from = new Date(ui.values[0] *1000),
+          to = new Date(ui.values[1] *1000);
+        $( "#match-date-range" ).text(formatDate(from) + " - " + formatDate(to));
+        self.fog_filter.start_time = [from, to];
+        self.redrawFog();
       }
     });
     $( "#match-date-range" ).text(
@@ -180,7 +367,9 @@ function Map(container_id){
       step: 1,
       values: filter_limits.timing,
       slide: function( event, ui ) {
-        $( "#match-time-range" ).text( ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+        $( "#match-time-range" ).text( ui.values[0] + " - " + ui.values[ 1 ] );
+        self.fog_filter.ward_time = ui.values;
+        self.redrawFog();
       }
     });
     $( "#match-time-range" ).text( $( "#match-time" ).slider( "values", 0 ) + " - " + $( "#match-time" ).slider( "values", 1 ) );  
